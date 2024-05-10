82cd7983948f4cd6eaeb78be3efc033ee19c9519
Callum Bryant <callum.l.bryant@gmail.com>
2024-05-10 23:43:11 +1000

    add trip saving

--- src/main/java/org/nixes/DataHelper.java
+++ src/main/java/org/nixes/DataHelper.java
@@ -34,4 +34,36 @@ public class DataHelper {
         scanner.close();
         return taps;
     }
+
+
+    public static void saveTripsCsv(List<Trip> trips, String fileName) throws IOException {
+        // StartedOnUTC, FinishedOnUTC, DurationSecs, FromStopId, ToStopId, ChargeAmount, CompanyId, BusID,
+        var header = "Started, Finished, DurationSecs, FromStopId, ToStopId, ChargeAmount, CompanyId, BusID, PAN, Status\n";
+        var lines = new ArrayList<String>();
+        lines.add(header);
+        for (var trip : trips) {
+            // columns are StartedOnUTC, FinishedOnUTC, DurationSecs, FromStopId, ToStopId, ChargeAmount, CompanyId, BusID, PAN
+            var line = String.format("%s, %s, %d, %s, %s, %s, %s, %s, %s, %s",
+                    trip.getStartedOnUTC().format(DATE_TIME_FORMATTER),
+                    trip.getFinishedOnUTC().format(DATE_TIME_FORMATTER),
+                    trip.getDurationSecs(),
+                    trip.getFromStopId(),
+                    trip.getToStopId(),
+                    trip.getChargeAmount().toString(),
+                    trip.getCompanyId(),
+                    trip.getBusID(),
+                    trip.getPrimaryAccountNumber(),
+                    trip.getStatus()
+            );
+            lines.add(line);
+        }
+
+        var file = new File(fileName);
+        var writer = new java.io.FileWriter(file);
+        for (var line : lines) {
+            writer.write(line);
+            writer.write("\n");
+        }
+        writer.close();
+    }
 }
--- src/main/java/org/nixes/TripProcessCommand.java
+++ src/main/java/org/nixes/TripProcessCommand.java
@@ -1,13 +1,32 @@
 package org.nixes;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class TripProcessCommand {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         var fileName = "taps.csv";
         var taps = DataHelper.loadTaps(fileName);
-        for (var tap: taps) {
-            System.out.println("Tap id: "+tap.getId());
+        for (var tap : taps) {
+            System.out.println("Tap id: " + tap.getId());
         }
+
+        var trips = new ArrayList<Trip>();
+        var testTrip = new Trip(
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                100,
+                "Stop1",
+                "Stop2",
+                new BigDecimal("3.25"),
+                "Company1",
+                "Bus37",
+                "TESTPAN",
+                TripStatus.COMPLETE
+        );
+        trips.add(testTrip);
+        DataHelper.saveTripsCsv(trips, "trips.csv");
     }
 }
