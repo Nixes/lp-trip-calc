300c49120530ceaa3fb1ac7227214081c4cd0df0
Callum Bryant <callum.l.bryant@gmail.com>
2024-05-12 00:10:41 +1000

    finalise command

--- src/main/java/org/nixes/TripProcessCommand.java
+++ src/main/java/org/nixes/TripProcessCommand.java
@@ -1,32 +1,17 @@
 package org.nixes;
 
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 
 public class TripProcessCommand {
     public static void main(String[] args) throws IOException {
         var fileName = "taps.csv";
+        var outputFileName = "trips.csv";
+        System.out.println("Processing trips from file: " + fileName);
         var taps = DataHelper.loadTaps(fileName);
-        for (var tap : taps) {
-            System.out.println("Tap id: " + tap.getId());
-        }
-
-        var trips = new ArrayList<Trip>();
-        var testTrip = new Trip(
-                LocalDateTime.now(),
-                LocalDateTime.now(),
-                100,
-                "Stop1",
-                "Stop2",
-                new BigDecimal("3.25"),
-                "Company1",
-                "Bus37",
-                "TESTPAN",
-                TripStatus.COMPLETE
-        );
-        trips.add(testTrip);
-        DataHelper.saveTripsCsv(trips, "trips.csv");
+        // would use a dependency injection framework to inject the TripProcessor in a bigger application context
+        var tripProcessor = new TripProcessor();
+        var trips = tripProcessor.processTrips(taps);
+        System.out.println("Saving trips to file: " + outputFileName);
+        DataHelper.saveTripsCsv(trips, outputFileName);
     }
 }
