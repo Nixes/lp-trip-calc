f41cc97291876f696aa29f6680f19a56fd380a18
Callum Bryant <callum.l.bryant@gmail.com>
2024-05-12 03:10:46 +1000

    add test case for double tapOn

--- src/test/java/TripProcessorTest.java
+++ src/test/java/TripProcessorTest.java
@@ -1,12 +1,10 @@
 import org.junit.jupiter.api.Test;
-import org.nixes.DataHelper;
-import org.nixes.Tap;
-import org.nixes.TripProcessor;
-import org.nixes.TripStatus;
+import org.nixes.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -82,6 +80,44 @@ public class TripProcessorTest {
         assertEquals("Bus1", trip.getBusID());
         assertEquals("PAN1", trip.getPrimaryAccountNumber());
         assertEquals(TripStatus.INCOMPLETE, trip.getStatus());
+    }
+
+    @Test
+    public void testDoubleTapOn() {
+        var taps = new ArrayList<Tap>();
+        var tap1 = new Tap("1", LocalDateTime.parse("01-01-2020 10:00:00", DataHelper.DATE_TIME_FORMATTER), true, "Stop1", "Company1", "Bus1", "PAN1");
+        var tap2 = new Tap("2", LocalDateTime.parse("01-01-2020 10:05:00", DataHelper.DATE_TIME_FORMATTER), true, "Stop2", "Company1", "Bus1", "PAN1");
+        taps.add(tap1);
+        taps.add(tap2);
+
+        var tripProcessor = new TripProcessor();
+        var trips = tripProcessor.processTrips(taps);
+
+        assertEquals(2, trips.size());
+        var trip = trips.get(0);
+        assertEquals(tap1.getDateTimeUTC(), trip.getStartedOnUTC());
+        assertEquals(tap1.getDateTimeUTC(), trip.getFinishedOnUTC());
+        assertEquals(0, trip.getDurationSecs());
+        assertEquals("Stop1", trip.getFromStopId());
+        assertEquals("Stop1", trip.getToStopId());
+        assertEquals(new BigDecimal("7.30"), trip.getChargeAmount());
+        assertEquals("Company1", trip.getCompanyId());
+        assertEquals("Bus1", trip.getBusID());
+        assertEquals("PAN1", trip.getPrimaryAccountNumber());
+        assertEquals(TripStatus.INCOMPLETE, trip.getStatus());
+
+        trip = trips.get(1);
+        assertEquals(tap2.getDateTimeUTC(), trip.getStartedOnUTC());
+        assertEquals(tap2.getDateTimeUTC(), trip.getFinishedOnUTC());
+        assertEquals(0, trip.getDurationSecs());
+        assertEquals("Stop2", trip.getFromStopId());
+        assertEquals("Stop2", trip.getToStopId());
+        assertEquals(new BigDecimal("5.50"), trip.getChargeAmount());
+        assertEquals("Company1", trip.getCompanyId());
+        assertEquals("Bus1", trip.getBusID());
+        assertEquals("PAN1", trip.getPrimaryAccountNumber());
+        assertEquals(TripStatus.INCOMPLETE, trip.getStatus());
+
     }
 
     @Test
