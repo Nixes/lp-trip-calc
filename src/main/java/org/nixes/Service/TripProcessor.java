6a640d6815e29f9c563a02f84f84e2b2af0db729
Callum Bryant <callum.l.bryant@gmail.com>
2024-05-15 15:21:34 +1000

    reduce function size

--- src/main/java/org/nixes/Service/TripProcessor.java
+++ src/main/java/org/nixes/Service/TripProcessor.java
@@ -20,7 +20,6 @@ public class TripProcessor implements ITripProcessor {
         this.tripPriceCalculator = tripPriceCalculator;
     }
 
-
     protected static boolean isTripCancelled(Tap tapOn, Tap tapOff) {
         return tapOn.getStopId().equals(tapOff.getStopId());
     }
@@ -42,64 +41,81 @@ public class TripProcessor implements ITripProcessor {
     }
 
     public List<Trip> processTrips(@NotNull List<Tap> inputTaps) {
-        // make a copy of the input list just to be safe we don't modify the original when sorting
-        var taps = new ArrayList<>(inputTaps);
-        // sort the taps by time ascending
-        taps.sort(Comparator.comparing(Tap::getDateTimeUTC));
-
+        var taps = sortTaps(inputTaps);
         var incompleteTrips = new HashMap<String, Tap>();
         var uncompletableTrips = new LinkedList<Tap>();
         var processedTrips = new LinkedList<Trip>();
+
         for (var tap: taps) {
-            if (tap.getIsTapOn()) {
-                // check if there is already a tapOn for this account number
-                var existingTap = incompleteTrips.get(tap.getPrimaryAccountNumber());
-                if (existingTap != null) {
-                    // if there is, then this tapOn is uncompletable
-                    uncompletableTrips.add(existingTap);
-                }
-
-                incompleteTrips.put(tap.getPrimaryAccountNumber(), tap);
-            } else {
-                var tapOn = incompleteTrips.get(tap.getPrimaryAccountNumber());
-                if (tapOn != null) {
-                    incompleteTrips.remove(tap.getPrimaryAccountNumber());
-                    var tripStatus = isTripCancelled(tapOn, tap) ? TripStatus.CANCELLED : TripStatus.COMPLETED;
-                    var chargeAmount = this.tripPriceCalculator.calculateChargeAmount(tapOn, tap, tripStatus);
-                    var tripDuration = Duration.between(tapOn.getDateTimeUTC(), tap.getDateTimeUTC()).toSeconds();
-
-                    // create a new tapOn
-                    var newTrip = new Trip(
-                            tapOn.getDateTimeUTC(),
-                            tap.getDateTimeUTC(),
-                            tripDuration,
-                            tapOn.getStopId(),
-                            tap.getStopId(),
-                            chargeAmount,
-                            tapOn.getCompanyId(),
-                            tapOn.getBusId(),
-                            tapOn.getPrimaryAccountNumber(),
-                            tripStatus
-                    );
-                    processedTrips.add(newTrip);
-                } else {
-                    uncompletableTrips.add(tap);
-                }
-            }
+            processTap(tap, incompleteTrips, uncompletableTrips, processedTrips);
         }
 
-        // add any remaining incomplete trips to the processed trips list
-        for (var tap: incompleteTrips.values()) {
-            processedTrips.add(createIncompleteTrip(tap));
-        }
-
-        for (var tap: uncompletableTrips) {
-            processedTrips.add(createIncompleteTrip(tap));
-        }
+        addRemainingIncompleteTrips(incompleteTrips, processedTrips);
+        addUncompletableTrips(uncompletableTrips, processedTrips);
 
         processedTrips.sort(Comparator.comparing(Trip::getStartedOnUTC));
 
         return processedTrips;
     }
 
+    private ArrayList<Tap> sortTaps(List<Tap> inputTaps) {
+        var taps = new ArrayList<>(inputTaps);
+        taps.sort(Comparator.comparing(Tap::getDateTimeUTC));
+        return taps;
+    }
+
+    private void processTap(Tap tap, HashMap<String, Tap> incompleteTrips, LinkedList<Tap> uncompletableTrips, LinkedList<Trip> processedTrips) {
+        if (tap.getIsTapOn()) {
+            processTapOn(tap, incompleteTrips, uncompletableTrips);
+        } else {
+            processTapOff(tap, incompleteTrips, uncompletableTrips, processedTrips);
+        }
+    }
+
+    private void processTapOn(Tap tap, HashMap<String, Tap> incompleteTrips, LinkedList<Tap> uncompletableTrips) {
+        var existingTap = incompleteTrips.get(tap.getPrimaryAccountNumber());
+        if (existingTap != null) {
+            uncompletableTrips.add(existingTap);
+        }
+
+        incompleteTrips.put(tap.getPrimaryAccountNumber(), tap);
+    }
+
+    private void processTapOff(Tap tap, HashMap<String, Tap> incompleteTrips, LinkedList<Tap> uncompletableTrips, LinkedList<Trip> processedTrips) {
+        var tapOn = incompleteTrips.get(tap.getPrimaryAccountNumber());
+        if (tapOn != null) {
+            incompleteTrips.remove(tap.getPrimaryAccountNumber());
+            var tripStatus = isTripCancelled(tapOn, tap) ? TripStatus.CANCELLED : TripStatus.COMPLETED;
+            var chargeAmount = this.tripPriceCalculator.calculateChargeAmount(tapOn, tap, tripStatus);
+            var tripDuration = Duration.between(tapOn.getDateTimeUTC(), tap.getDateTimeUTC()).toSeconds();
+
+            var newTrip = new Trip(
+                    tapOn.getDateTimeUTC(),
+                    tap.getDateTimeUTC(),
+                    tripDuration,
+                    tapOn.getStopId(),
+                    tap.getStopId(),
+                    chargeAmount,
+                    tapOn.getCompanyId(),
+                    tapOn.getBusId(),
+                    tapOn.getPrimaryAccountNumber(),
+                    tripStatus
+            );
+            processedTrips.add(newTrip);
+        } else {
+            uncompletableTrips.add(tap);
+        }
+    }
+
+    private void addRemainingIncompleteTrips(HashMap<String, Tap> incompleteTrips, LinkedList<Trip> processedTrips) {
+        for (var tap: incompleteTrips.values()) {
+            processedTrips.add(createIncompleteTrip(tap));
+        }
+    }
+
+    private void addUncompletableTrips(LinkedList<Tap> uncompletableTrips, LinkedList<Trip> processedTrips) {
+        for (var tap: uncompletableTrips) {
+            processedTrips.add(createIncompleteTrip(tap));
+        }
+    }
 }
